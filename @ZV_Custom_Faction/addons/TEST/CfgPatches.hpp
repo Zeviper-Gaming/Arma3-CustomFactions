ab439ee4f8ee09cc1b50799de0cdd6a938a401ce
floriandelrieu <florian.delrieu@free.fr>
2023-06-08 06:25:18 +0200

    [v0.4] add Test folder

---
+++ @ZV_Custom_Faction/addons/TEST/CfgPatches.hpp
@@ -0,0 +1,47 @@
+class CfgPatches {
+    class B_TEST {
+        units[] = {
+            "B_TEST_Unit_01",
+            "B_TEST_Unit_02"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "A3_Characters_F",
+            "rhsusf_c_troops",
+            "CUP_Creatures_Military_BAF",
+            "CUP_Creatures_Military_Germany",
+            "CUP_Creatures_Military_NAPA",
+            "CUP_Creatures_Military_PMC",
+            "CUP_Creatures_Military_Taki",
+            "CUP_Creatures_Military_TakiInsurgents",
+            "CUP_Creatures_Military_USMC",
+            "CUP_Creatures_Military_ACR",
+            "CUP_Creatures_Military_CDF",
+            "CUP_Creatures_Military_Chedaki",
+            "CUP_Creatures_Military_Delta",
+            "CUP_Creatures_Military_RACS",
+            "CUP_Creatures_Military_SLA",
+            "CUP_Creatures_Military_USArmy",
+            "CUP_Creatures_Military_HIL",
+            "aceax_ingame",
+            "A3_Sounds_F",
+            "A3_Sounds_F_Exp",
+            "mcc_sandbox",
+            "ace_gforces",
+            "ace_movement",
+            "ace_medical_feedback",
+            "CUP_Creatures_Military_Russia",
+            "A3_Data_F",
+            "A3_Data_F_Curator",
+            "A3_Air_F_Heli",
+            "ace_thermals",
+            "zen_attributes",
+            "ace_refuel",
+            "CUP_Core",
+            "cba_xeh"
+        };
+        author = "Florian";
+        authors[] = {"Florian"};
+    };
+};
