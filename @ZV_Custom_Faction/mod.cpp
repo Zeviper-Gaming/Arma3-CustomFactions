0c09a3f938bf124659022cd11ba20d7f4d67d3a0
floriandelrieu <florian.delrieu@free.fr>
2023-06-09 06:30:17 +0200

    adding mod.cpp

---
+++ @ZV_Custom_Faction/mod.cpp
@@ -0,0 +1,7 @@
+name		= "[ZV] custom faction 0.9.1";
+author		= "Zeviper";
+actionName	= "Go to github"
+action		= "https://github.com/Zeviper-Gaming/Arma3-CustomFactions";
+hideName	= 0;
+hidepicture	= 0;
+overview	= "There are my customized faction in"
